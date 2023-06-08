92d88703f1989f310056351dcca1de1434ee7a16
datodvp <gaguadato98@gmail.com>
2023-06-08 14:44:36 +0400

    feat: translate reset password and login pages

--- src/components/Auth/ResetPassword.vue
+++ src/components/Auth/ResetPassword.vue
@@ -36,27 +36,26 @@ const onSubmit = async (values) => {
 <template>
     <PopUpCard>
         <div class="flex flex-col items-center justify-center w-full">
-            <h1 class="text-3xl font-medium">Create new password</h1>
-            <p class="text-[#6C757D] mt-3 max-w-[21rem] text-center">Your new password must be different from
-                previous used passwords</p>
+            <h1 class="text-3xl font-medium">{{ $t("auth.create_new_password") }}</h1>
+            <p class="text-[#6C757D] mt-3 max-w-[21rem] text-center">{{ $t("auth.different_password") }}</p>
             <Form @submit="onSubmit" class="w-full px-4 md:px-0">
 
-                <TextInput type="password" label="Password" name="password"
-                    placeholder="At least 8 & max.15 lower case characters" v-model="password"
+                <TextInput type="password" :label="$t('auth.password')" name="password"
+                    :placeholder="$t('auth.password_placeholder')" v-model="password"
                     :rules="{ required: true, min: 8, max: 15, latin: true }" />
 
-                <TextInput type="password" label="Confirm password" name="password_confirmation"
-                    placeholder="Confirm password" v-model="passwordConfirmation"
+                <TextInput type="password" :label="$t('auth.confirm_password')" name="password_confirmation"
+                    :placeholder="$t('auth.confirm_password_placeholder')" v-model="passwordConfirmation"
                     :rules="{ required: true, confirmed: 'password' }" />
 
                 <ServerErrorMessage :errorMessage="errorMessage" />
 
                 <PrimaryButton class="mt-6">
-                    <button>Reset password</button>
+                    <button>{{ $t('auth.reset_password') }}</button>
                 </PrimaryButton>
 
                 <RouterLink :to="{ name: 'login' }" class="flex justify-center items-center mt-8 text-[#6C757D] gap-3">
-                    <IconBackArrow />Back to log in
+                    <IconBackArrow />{{ $t('auth.back_to_log_in') }}
                 </RouterLink>
 
             </Form>
--- src/components/Auth/TheLogin.vue
+++ src/components/Auth/TheLogin.vue
@@ -42,39 +42,40 @@ const loginGoogle = async () => {
 <template>
     <PopUpCard>
         <div class="flex flex-col items-center justify-center w-full">
-            <h1 class="p-4 text-3xl font-medium">Log in to your account</h1>
-            <p class="p-4 text-[#6C757D] mt-3">Welcome back! Please enter your details.</p>
+            <h1 class="p-4 text-3xl font-medium">{{ $t("auth.log_in_to_your_account") }}</h1>
+            <p class="p-4 text-[#6C757D] mt-3">{{ $t("auth.welcome_back") }}</p>
             <Form @submit="authorize" class="w-full px-4 md:px-0">
-                <TextInput label="Email or Username" name="login" placeholder="Enter your email or username" v-model="login"
-                    :rules="{ required: true }" />
-                <TextInput type="password" label="Password" name="password" placeholder="Password" v-model="password"
+                <TextInput :label="$t('auth.email_or_username')" name="login"
+                    :placeholder="$t('auth.email_or_username_placeholder')" v-model="login" :rules="{ required: true }" />
+                <TextInput type="password" :label="$t('auth.password')" name="password"
+                    :placeholder="$t('auth.password_placeholder')" v-model="password"
                     :rules="{ required: true, latin: true, min: 8, max: 15 }" />
                 <div class="flex justify-between mt-4">
                     <label class="flex items-center gap-2 cursor-pointer w-fit">
                         <Field type="checkbox" name="remember" v-model="remember_me" class="w-[16px] h-[16px]" />
-                        Remember me
+                        {{ $t("auth.remember_me") }}
                     </label>
                     <RouterLink :to="{ name: 'forgotPassword' }"
-                        class="text-[#0D6EFD] underline flex justify-center items-center">Forgot password
+                        class="text-[#0D6EFD] underline flex justify-center items-center">{{ $t("auth.forgot_password") }}
                     </RouterLink>
                 </div>
 
                 <ServerErrorMessage :errorMessage="errorMessage" />
 
                 <PrimaryButton class="mt-4">
-                    <button>Sign in</button>
+                    <button>{{ $t("auth.sign_in") }}</button>
                 </PrimaryButton>
 
                 <SecondaryButton @click="loginGoogle" class="mt-4">
 
                     <button type="button">
-                        <IconGoogle class="mr-2" />Sign in with Google
+                        <IconGoogle class="mr-2" />{{ $t("auth.sign_in_google") }}
                     </button>
                 </SecondaryButton>
 
                 <div class="flex justify-center gap-1 mt-8">
-                    <p>Don't have an account?</p>
-                    <RouterLink to="/sign-up" class="text-[#0D6EFD] underline">Sign up</RouterLink>
+                    <p>{{ $t("auth.dont_have_account") }}</p>
+                    <RouterLink to="/sign-up" class="text-[#0D6EFD] underline">{{ $t("auth.sign_up") }}</RouterLink>
                 </div>
 
             </Form>
